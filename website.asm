622010d0041a5bc19eabe19a5a53b9aaa61da135
onworks14 <150022695+onworks14@users.noreply.github.com>
2023-12-07 10:12:52 +0000

    Update website.asm

--- website.asm
+++ website.asm
@@ -10,10 +10,10 @@ SYS_close equ 3
 
 AF_INET equ 2
 SOCK_STREAM equ 1
+INADDR_ANY equ 0
 
 STDOUT equ 1
 STDERR equ 2
-INADDR_ANY equ 0
 
 EXIT_SUCCESS equ 0
 EXIT_FAILURE equ 1
@@ -44,18 +44,18 @@ macro syscall3 number, a, b, c
   syscall
 }
 
-macro write fd, buf, count 
+macro write fd, buf, count
 {
-  mov rax, SYS_write
-  mov rdi, fd
-  mov rsi, buf
-  mov rdx, start_len
-  syscall
+   mov rax, SYS_write
+   mov rdi, fd
+   mov rsi, buf
+   mov rdx, count
+   syscall
 }
 
 macro close fd
 {
- syscall1 SYS_close, fd
+  syscall1 SYS_close, fd
 }
 
 macro socket domain, type, protocol
@@ -84,66 +84,61 @@ macro accept sockfd, addr, addrlen
 
 macro exit code
 {
-  mov rax, SYS_exit
-  mov rdi, code
-  syscall
+   mov rax, SYS_exit
+   mov rdi, 0
+   syscall
 }
 
 segment readable executable
+
 entry main
 main:
-  write STDOUT, start, start_len
-  
-  write STDOUT, socket_trace_msg, socket_trace_msg_len
-  socket AF_INET, SOCK_STREAM, 0
-  ;; socket 69, 420, 0
-  cmp rax, 0
-  jl error
-  mov qword [sockfd], rax
+   write STDOUT, start, start_len
 
-  write STDOUT, bind_trace_msg, bind_trace_msg_len
-  mov word [servaddr.sin_family], AF_INET
-  mov dword [servaddr.sin_addr], INADDR_ANY
-  mov word [servaddr.sin_port], 14619
-  bind [sockfd], servaddr.sin_family, sizeof_servaddr
-  cmp rax, 0
-  jl error
+   write STDOUT, socket_trace_msg, socket_trace_msg_len
+   socket AF_INET, SOCK_STREAM, 0
+   ;;socket 69, 420, 0
+   cmp rax, 0
+   jl error
+   mov qword [sockfd], rax
 
-  write STDOUT, listen_trace_msg, listen_trace_msg_len
-  listen [sockfd], MAX_CONN
-  cmp rax, 0
-  jl error
+   write STDOUT, bind_trace_msg, bind_trace_msg_len
+   mov word [servaddr.sin_family], AF_INET
+   mov word [servaddr.sin_port], 14619
+   mov dword [servaddr.sin_family], INADDR_ANY
+   bind [sockfd], servaddr.sin_family, sizeof_servaddr
+   cmp rax, 0
+   jl error
+
+   write STDOUT, listen_trace_msg, listen_trace_msg_len
+   listen [sockfd], MAX_CONN
+   cmp rax, 0
+   jl error
 
 next_request:
-  write STDOUT, accept_trace_msg, accept_trace_msg_len
-  accept [sockfd], cliaddr.sin_family, cliaddr_len
-  cmp rax, 0
-  jl error
+   write STDOUT, accept_trace_msg, accept_trace_msg_len
+   accept [sockfd], cliaddr.sin_family, cliaddr_len
+   cmp rax, 0
+   jl error
 
-  mov qword[connfd], rax
+   mov qword [connfd], rax
+
+   write [connfd], response, response_len 
+
+   jmp next_request
   
-  write [connfd], response, response_len
-
-  jmp next_request
-  
-
-  write STDOUT, ok_msg, ok_msg_len
-  close [connfd]
-  close [sockfd]
-  exit  0
+   write STDOUT, ok_msg, ok_msg_len
+   close [connfd]
+   close [sockfd]
+   exit 0
 
 error:
-  write STDERR, error_msg, error_msg_len
-  close [connfd]
-  close [sockfd]
-  exit 1
+    write STDERR, error_msg, error_msg_len
+    close [connfd]
+    close [sockfd]
+    exit 1
 
-struc point x, y
-{
-  .x db x,
-  .y db y
-}
-
+    
 segment readable writeable
 
 struc servaddr_in
@@ -152,17 +147,13 @@ struc servaddr_in
   .sin_port dw 0
   .sin_addr dd 0
   .sin_zero dq 0
-  ;;.size = .sin_family - $
 }
 
 sockfd dq -1
 connfd dq -1
-;;servaddr.sin_family dw 0
-;;servaddr.sin_port dw 0
-;;servaddr.sin_addr dd 0
-;;servaddr.sin_zero dq 0
-servaddr servaddr_in 
-sizeof_servaddr =  $ - servaddr.sin_family
+
+servaddr servaddr_in
+sizeof_servaddr = $ - servaddr.sin_family
 cliaddr servaddr_in
 cliaddr_len dd sizeof_servaddr
 
@@ -176,17 +167,17 @@ response db "HTTP/1.1 200 OK", 13, 10
          db "<h1>Hello from flat assembler!</h1>", 10
 response_len = $ - response
 
-start db "INFO: Starting Web Server!" , 10
+start db "INFO: Starting web server!", 10
 start_len = $ - start
-ok_msg db "INFO: OK!" , 10
+ok_msg db "INFO: OK!", 10
 ok_msg_len = $ - ok_msg
-socket_trace_msg db "INFO: Creating a socket..." , 10
+socket_trace_msg db "INFO: Creating a socket...", 10
 socket_trace_msg_len = $ - socket_trace_msg
-bind_trace_msg db "INFO: Binding the socket..." , 10
+bind_trace_msg db "INFO: Binding the socket...", 10
 bind_trace_msg_len = $ - bind_trace_msg
 listen_trace_msg db "INFO: Listening to the socket...", 10
-listen_trace_msg_len = $ - listen_trace_msg
+listen_trace_msg_len = $ -listen_trace_msg
 accept_trace_msg db "INFO: Waiting for client connections...", 10
 accept_trace_msg_len = $ - accept_trace_msg
-error_msg db "ERROR!" , 10
+error_msg db "ERROR!", 10
 error_msg_len = $ - error_msg
